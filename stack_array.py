806090a733da00036b6280d79b31a07ff0c2a1c1
Jared Swislow <jayswis2002@gmail.com>
2021-01-28 13:34:42 -0800

    Imported stack class and tests from lab 2

--- stack_array.py
+++ stack_array.py
@@ -1,1 +1,56 @@
+class Stack:
+    '''Implements an efficient last-in first-out Abstract Data Type using a Python List'''
 
+    #Creates an empty stack with a specified capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.items = [None]*capacity
+        self.num_items = 0
+
+    #self -> boolean
+    #Returns true if the stack is empty, and false if it is not
+    def is_empty(self):
+        if self.num_items == 0:
+            return True
+        return False
+
+    #self -> boolean
+    #Returns true if stack is full, and false otherwise
+    def is_full(self):
+        if self.num_items == self.capacity:
+            return True
+        else:
+            return False
+
+    #self, literal -> None
+    #Pushes an item onto the stack
+    #If stack is already full, raises IndexError
+    def push(self, item):
+        if self.is_full():
+            raise IndexError
+        self.items[self.num_items] = item
+        self.num_items += 1
+
+
+    #self -> literal
+    #Pops an item from the stack, removing it and returning its value
+    #If the stack is empty, raises IndexError
+    def pop(self):
+        if self.is_empty():
+            raise IndexError
+        poppedValue = self.items[self.num_items - 1]
+        self.num_items -= 1
+        return poppedValue
+
+    #self -> literal
+    #Peeks at an item from the top of the stack, returning it's value
+    #If the stack is empty, raises IndexError
+    def peek(self):
+        if self.is_empty():
+            raise IndexError
+        return self.items[self.num_items - 1]
+
+    #self -> integer
+    #Returns the size of the stack
+    def size(self):
+        return self.num_items
--- stack_tests.py
+++ stack_tests.py
@@ -1,1 +1,54 @@
+import unittest
 
+#Submit with linked stack commented out
+from stack_array import Stack
+#from stack_linked import Stack
+
+class TestLab2(unittest.TestCase):
+    def test_simple(self):
+        stack = Stack(3)
+        stack.push(0)
+        self.assertFalse(stack.is_empty())
+        self.assertFalse(stack.is_full())
+        self.assertEqual(stack.peek(), 0)
+        stack.push(1)
+        self.assertEqual(stack.peek(), 1)
+        stack.push(2)
+        self.assertEqual(stack.size(),3)
+        stack.pop()
+        stack.pop()
+        self.assertEqual(stack.peek(), 0)
+        self.assertEqual(stack.size(),1)
+        self.assertEqual(stack.pop(), 0)
+        self.assertEqual(stack.size(),0)
+
+    def test_EmptyAndFull(self):
+        stack = Stack(1)
+        self.assertTrue(stack.is_empty())
+        self.assertFalse(stack.is_full())
+        stack.push(5)
+        self.assertFalse(stack.is_empty())
+        self.assertTrue(stack.is_full())
+
+    def test_NoneAsData(self):
+        stack = Stack(3)
+        stack.push(None)
+        self.assertEqual(stack.peek(), None)
+        self.assertEqual(stack.size(), 1)
+        stack.pop()
+        self.assertEqual(stack.size(), 0)
+    
+    def test_Errors(self):
+        stack = Stack(1)
+        with self.assertRaises(IndexError):
+            stack.pop()
+        with self.assertRaises(IndexError):
+            stack.peek()
+        stack.push(1)
+        with self.assertRaises(IndexError):
+            stack.push(2)
+
+
+
+if __name__ == '__main__': 
+    unittest.main()
